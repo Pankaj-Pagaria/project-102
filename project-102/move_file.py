5bb39ac0e8f93da0c2d1a3f6e1e23afe7fbc1963
Pankaj-Pagaria <149296800+Pankaj-Pagaria@users.noreply.github.com>
2024-06-03 11:11:54 +0300

    Add files via upload

---
+++ project-102/move_file.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+from_dir = "C:/Users/DELL/Downloads"
+to_dir = "C:/Users/DELL/Downloads/example"
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+
+for i in list_of_files:
+    name, ext = os.path.splitext(i)
+    if ext == " ":
+        continue
+    if ext in [ '.txt', '.doc', '.docx', '.pdf']:
+        path1 = from_dir + "/" + i
+        path2 = to_dir + "/" + "Document_files"
+        path3 = to_dir + '/' + 'Document_files' + '/' + i
+        if os.path.exists(path2):
+            print(f"moving {i}...")
+            shutil.move(path1,path3)
+        else:
+            os.makedirs(path2)
+            print(f"moving {i}...")
+            shutil.move(path1,path3)
